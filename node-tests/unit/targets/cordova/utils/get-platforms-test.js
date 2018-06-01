cb5ebf6e5dd36151a0d3d7652ac569b18d3896df
Jordan Yee <jordanyee@users.noreply.github.com>
2018-06-01 17:54:39 -0400

    style(get-platforms): Modernize Cordova target util test

--- node-tests/unit/targets/cordova/utils/get-platforms-test.js
+++ node-tests/unit/targets/cordova/utils/get-platforms-test.js
@@ -1,24 +1,24 @@
-var td              = require('testdouble');
-var expect          = require('../../../../helpers/expect');
-var getCordovaPath  = require('../../../../../lib/targets/cordova/utils/get-path');
+const td              = require('testdouble');
+const expect          = require('../../../../helpers/expect');
+const getCordovaPath  = require('../../../../../lib/targets/cordova/utils/get-path');
 
-var path            = require('path');
+const path            = require('path');
 
-var mockProject     = require('../../../../fixtures/corber-mock/project');
+const mockProject     = require('../../../../fixtures/corber-mock/project');
 
 describe('Get Added Platforms Util', function() {
   context('when project has platforms.json, use it over package.json', function() {
     let subject;
 
     beforeEach(function() {
-      var cordovaPath = getCordovaPath(mockProject.project.root);
-      var platformsPath = path.join(cordovaPath, 'platforms/platforms.json');
-      var packagePath = path.join(cordovaPath, 'package.json');
+      let cordovaPath = getCordovaPath(mockProject.project.root);
+      let platformsPath = path.join(cordovaPath, 'platforms/platforms.json');
+      let packagePath = path.join(cordovaPath, 'package.json');
 
       td.replace(platformsPath, { 'ios': '4.3.1' });
       td.replace(packagePath, { 'cordova': { 'platforms': ['android'] } });
 
-      var getAddedPlatforms = require('../../../../../lib/targets/cordova/utils/get-platforms');
+      let getAddedPlatforms = require('../../../../../lib/targets/cordova/utils/get-platforms');
 
       subject = getAddedPlatforms(mockProject.project);
     });
@@ -33,17 +33,16 @@ describe('Get Added Platforms Util', function() {
   });
 
   context('fallback to package.json when project has no platforms.json', function() {
-
     let subject;
 
     beforeEach(function() {
-      var cordovaPath = getCordovaPath(mockProject.project.root);
-      var packagePath = path.join(cordovaPath, 'package.json');
+      let cordovaPath = getCordovaPath(mockProject.project.root);
+      let packagePath = path.join(cordovaPath, 'package.json');
 
       td.replace(packagePath, { 'cordova': { 'platforms': ['android'] } });
 
       // Context relies on mockProject not including a platform.json.
-      var getAddedPlatforms = require('../../../../../lib/targets/cordova/utils/get-platforms');
+      let getAddedPlatforms = require('../../../../../lib/targets/cordova/utils/get-platforms');
 
       subject = getAddedPlatforms(mockProject.project);
     });
@@ -58,12 +57,11 @@ describe('Get Added Platforms Util', function() {
   });
 
   context('when project has no platforms.json or package.json', function() {
-
     let subject;
 
     beforeEach(function() {
       // Context relies on mockProject not including a platform.json.
-      var getAddedPlatforms = require('../../../../../lib/targets/cordova/utils/get-platforms');
+      let getAddedPlatforms = require('../../../../../lib/targets/cordova/utils/get-platforms');
 
       subject = getAddedPlatforms(mockProject.project);
     });
