2da37cc1f4bed6b7935f75cf27e24422359c14dc
Jordan Yee <jordanyee@users.noreply.github.com>
2018-06-01 16:00:01 -0400

    style(update-watchman-config): Modernize code

--- node-tests/unit/frameworks/ember/tasks/update-watchman-config-test.js
+++ node-tests/unit/frameworks/ember/tasks/update-watchman-config-test.js
@@ -1,14 +1,15 @@
-var td              = require('testdouble');
-var expect          = require('../../../../helpers/expect');
-var path            = require('path');
-var mockProject     = require('../../../../fixtures/corber-mock/project');
-var Promise         = require('rsvp').Promise;
+const WatchmanConfig = require('../../../../../lib/frameworks/ember/tasks/update-watchman-config');
 
-var WatchmanConfig  = require('../../../../../lib/frameworks/ember/tasks/update-watchman-config');
-var fsUtils         = require('../../../../../lib/utils/fs-utils');
+const path           = require('path');
+const td             = require('testdouble');
+const Promise        = require('rsvp').Promise;
+
+const expect         = require('../../../../helpers/expect');
+const mockProject    = require('../../../../fixtures/corber-mock/project');
+const fsUtils        = require('../../../../../lib/utils/fs-utils');
 
 describe('Update Watchman Config Task', function() {
-  var watchmanTask;
+  let watchmanTask;
 
   beforeEach(function() {
     watchmanTask = new WatchmanConfig({
@@ -20,9 +21,9 @@ describe('Update Watchman Config Task', function() {
     td.reset();
   });
 
-  it('attempts to read watchmanconfig', function() {
-    var actualPath;
-    var expectedPath = path.join(mockProject.project.root, '.watchmanconfig');
+  it('reads .watchmanconfig', function() {
+    let expectedPath = path.join(mockProject.project.root, '.watchmanconfig');
+    let actualPath;
 
     td.replace(fsUtils, 'write');
     td.replace(fsUtils, 'read', function(path) {
@@ -36,13 +37,11 @@ describe('Update Watchman Config Task', function() {
   });
 
   it('adds corber to existing ignore_dirs array', function() {
-    var writeContents;
-    var expectedWrite = '{"ignore_dirs":["tmp","dist","corber"]}';
+    let writeContents;
+    let expectedWrite = '{"ignore_dirs":["tmp","dist","corber"]}';
 
     td.replace(fsUtils, 'read', function(path) {
-      return new Promise(function(resolve) {
-        resolve('\{"ignore_dirs": ["tmp", "dist"]\}');
-      });
+      return Promise.resolve('\{"ignore_dirs": ["tmp", "dist"]\}');
     });
 
     td.replace(fsUtils, 'write', function(path, contents) {
@@ -56,12 +55,10 @@ describe('Update Watchman Config Task', function() {
   });
 
   it('creates an ignore_dirs array if it does not exist', function() {
-    var writeContents;
+    let writeContents;
 
     td.replace(fsUtils, 'read', function(path) {
-      return new Promise(function(resolve) {
-        resolve('\{\}');
-      });
+      return Promise.resolve('\{\}');
     });
 
     td.replace(fsUtils, 'write', function(path, contents) {
@@ -75,13 +72,11 @@ describe('Update Watchman Config Task', function() {
   });
 
   it('does not duplicate content', function() {
-    var writeContents;
-    var expectedWrite = '{"ignore_dirs":["tmp","dist","corber"]}';
+    let writeContents;
+    let expectedWrite = '{"ignore_dirs":["tmp","dist","corber"]}';
 
     td.replace(fsUtils, 'read', function(path) {
-      return new Promise(function(resolve) {
-        resolve('\{"ignore_dirs": ["tmp", "dist", "corber"]\}');
-      });
+      return Promise.resolve('\{"ignore_dirs": ["tmp", "dist", "corber"]\}');
     });
 
     td.replace(fsUtils, 'write', function(path, contents) {
